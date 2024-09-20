2519937297949928d10587a8908e0b554762cc5b
alvina-yang <122567598+alvina-yang@users.noreply.github.com>
2024-09-20 18:01:37 -0400

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/3d_portfolio/',
-})
+  base: '/3d_portfolio/', 
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
+});
